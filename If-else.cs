7df01ad3772aebf1f359797f41b3b9b631cd5f96
Andrey <93141508+svpra@users.noreply.github.com>
2021-11-10 20:39:05 +0300

    Create If-else.cs

---
+++ If-else.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program {
+    static void Main() {
+        if x = "5" {
+            Console.WriteLine(5);
+            }
+        if x = "4" {
+            Console.WriteLine(4);
+            }
+        if x = "3" {
+            Console.WriteLine(3);
+            }
+        if x = "2" {
+            Console.WriteLine(2);
+            }
+        if x = "1" {
+            Console.WriteLine(1);
+            }
+        }
+    }
