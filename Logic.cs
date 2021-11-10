78fca391588e27781fe6a6444d9eb613a2fe371d
Andrey <93141508+svpra@users.noreply.github.com>
2021-11-10 20:41:40 +0300

    Create Logic.cs

---
+++ Logic.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program {
+    static void Main() {
+        if x = "5"
+        || x = "4"
+        || x = "3"
+        || x = "2"
+        || x = "1"
+
+        Console.WriteLine("huy");
+        }
+    }
