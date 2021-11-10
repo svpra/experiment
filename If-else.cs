ee2e362e98f4cca98869c014f3ef54e1e30b1c91
Andrey <93141508+svpra@users.noreply.github.com>
2021-11-10 20:43:11 +0300

    Update If-else.cs

--- If-else.cs
+++ If-else.cs
@@ -3,19 +3,21 @@ using System;
 class Program {
     static void Main() {
         if x = "5" {
-            Console.WriteLine(5);
+            Console.WriteLine("huy);
             }
         if x = "4" {
-            Console.WriteLine(4);
+            Console.WriteLine("huy");
             }
         if x = "3" {
-            Console.WriteLine(3);
+            Console.WriteLine("huy");
             }
         if x = "2" {
-            Console.WriteLine(2);
+            Console.WriteLine("huy");
             }
         if x = "1" {
-            Console.WriteLine(1);
+            Console.WriteLine("huy");
             }
+        else:
+            Console.WriteLine("huynya")
         }
     }
